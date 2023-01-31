063219edcacb800e5f9f36ccd2323379b7d35874
Simone Flavio <smoxy@users.noreply.github.com>
2023-01-31 14:52:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,14 +12,12 @@ from datetime import datetime as dt
 from selenium import webdriver
 
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, TimeoutException, StaleElementReferenceException, ElementClickInterceptedException, ElementNotInteractableException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, StaleElementReferenceException
 
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 
