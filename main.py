794449c0e12f5898582619d43f8c0bc0f935df22
Simone Flavio <smoxy@users.noreply.github.com>
2023-02-01 10:33:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -56,12 +56,14 @@ def lan_sharing(power: bool):
     if power:
         try:
             with subprocess.Popen(["sudo", "wg-quick", "up", "wg1"], stdin=subprocess.PIPE) as proc:
-                proc.stdin.write(sudo_pwd)
+                proc.stdin.write(sudo_pwd.encode())
                 proc.stdin.flush()
             sleep(1)
         except:
             pass
-        subprocess.Popen(["sudo", "ip", "link", "set", eth0, "up"])
+        with subprocess.Popen(["sudo", "ip", "link", "set", eth0, "up"], stdin=subprocess.PIPE) as proc:
+            proc.stdin.write(sudo_pwd.encode())
+            proc.stdin.flush()
         sleep(1)
         subprocess.Popen(["sudo", "ifconfig", eth0, "up"])
         sleep(1)
@@ -69,7 +71,7 @@ def lan_sharing(power: bool):
         logging.info(f"\t{t_now}\tWireguard and {eth0} turned ON!\n")
     else:
         with subprocess.Popen(["sudo", "ifconfig", eth0, "down"], stdin=subprocess.PIPE) as proc:
-            proc.stdin.write(sudo_pwd)
+            proc.stdin.write(sudo_pwd.encode())
             proc.stdin.flush()
         sleep(1)
         subprocess.Popen(["sudo", "ip", "link", "set", eth0, "down"])
@@ -178,7 +180,7 @@ def connectV2(chromium: bool, headless: bool, noimage: bool, nolocation: bool, v
 
     if "linux" in platform.platform().lower():
         with subprocess.Popen(["sudo", "ip", "link", "set", wlan0, "down"], stdin=subprocess.PIPE) as proc:
-            proc.stdin.write(sudo_pwd)
+            proc.stdin.write(sudo_pwd.encode())
             proc.stdin.flush()
         sleep(5)
 
@@ -263,7 +265,7 @@ def renew_connection(mac: int, wait=True, log=False, verbose=False):
                 MAC = MACs[mac]
 
         with subprocess.Popen(["sudo", "ifconfig", wlan0, "down"], stdin=subprocess.PIPE) as proc:
-            proc.stdin.write(sudo_pwd)
+            proc.stdin.write(sudo_pwd.encode())
             proc.stdin.flush()
         sleep(3)
 
@@ -275,12 +277,12 @@ def renew_connection(mac: int, wait=True, log=False, verbose=False):
         sleep(5)
 
         with subprocess.Popen(["sudo", "ifconfig", wlan0, "up"], stdin=subprocess.PIPE) as proc:
-            proc.stdin.write(sudo_pwd)
+            proc.stdin.write(sudo_pwd.encode())
             proc.stdin.flush()
         sleep(5)
 
         with subprocess.Popen(["sudo", "dhclient", "-r", wlan0], stdin=subprocess.PIPE) as proc:
-            proc.stdin.write(sudo_pwd)
+            proc.stdin.write(sudo_pwd.encode())
             proc.stdin.flush()
         t_now = (dt.now()).strftime("%Y/%m/%d - %H:%M:%S")
         if verbose:
