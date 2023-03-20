0f961cbf090c361bd41a454196311a7e6a752671
Simone Flavio <smoxy@users.noreply.github.com>
2023-03-20 19:23:04 +0100

    added super user password field

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 USR = "USERNAME"
 PWD = "PASSWORD"
+sudo_pwd = "sudo password"
 MACs = ["XX:XX:XX:XX:XX:XX", "XX:XX:XX:XX:XX:XX", "XX:XX:XX:XX:XX:XX", "XX:XX:XX:XX:XX:XX", "XX:XX:XX:XX:XX:XX"]
 wlan0 = "wlp3s0"
 eth0 = "enp4s0"
